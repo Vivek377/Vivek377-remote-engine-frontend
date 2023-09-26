5502f279ae0314059cb4ff0a23397c9d304baa84
Vivek377 <vk640890@gmail.com>
2023-09-26 09:27:21 +0530

    frontend

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,17 +4,14 @@ export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [isAuth, setIsAuth] = useState(false);
-  const [token, setToken] = useState(null);
 
   const loginUser = (t) => {
     setIsAuth(true);
-    setToken(t);
   };
 
   const value = {
     loginUser,
     isAuth,
-    token,
     setIsAuth,
   };
 
